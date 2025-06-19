9306de509832f96435e4bc0bd36d79b2d9f64cf5
Faddellin <belovodchenko2005@mail.ru>
2025-06-19 18:58:00 +0700

    init

---
+++ src/main/java/org/DAD/DungeonAndDilemmas.java
@@ -0,0 +1,11 @@
+package org.DAD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DungeonAndDilemmas {
+    public static void main(String[] args) {
+        SpringApplication.run(DungeonAndDilemmas.class, args);
+    }
+}
